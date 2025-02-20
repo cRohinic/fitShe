59849e8484e9d18e79102b79cf8e3d7b23dfd0ce
cRohinic <150899608+cRohinic@users.noreply.github.com>
2025-02-20 21:19:21 +0530

    Added project files

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -25,7 +25,7 @@ const Pricing = () => {
   ];
   return (
     <section className="pricing">
-      <h1>ELITE EDGE FITNESS PLANS</h1>
+      <h1>fitShe PLANS</h1>
       <div className="wrapper">
         {pricing.map((element) => {
           return (
